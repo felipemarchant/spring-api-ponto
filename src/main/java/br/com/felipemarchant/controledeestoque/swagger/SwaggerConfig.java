dd7d328cf1d7dad1a9ca40cd94fc507702b78349
Felipe Marchant F. Soares <felipemarchant.dev@gmail.com>
2021-07-19 11:53:31 -0300

    Correção do increment do id

--- src/main/java/br/com/felipemarchant/controledeestoque/swagger/SwaggerConfig.java
+++ src/main/java/br/com/felipemarchant/controledeestoque/swagger/SwaggerConfig.java
@@ -10,11 +10,13 @@ import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.schema.ModelRef;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
+import springfox.documentation.service.Parameter;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.util.Collections;
+import java.util.List;
 
 @Configuration
 @EnableSwagger2
@@ -22,21 +24,23 @@ public class SwaggerConfig {
 
     @Bean
     public Docket apiAdmin() {
+        List<Parameter> params =
+                Collections.singletonList(
+                        new ParameterBuilder()
+                                .name("Authorization")
+                                .description("Header para Token JWT")
+                                .modelRef(new ModelRef("string"))
+                                .parameterType("header")
+                                .required(false)
+                                .build());
+        params = Collections.emptyList();
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
                 .apis(RequestHandlerSelectors.basePackage(ControledeestoqueApplication.class.getPackageName()))
                 .paths(PathSelectors.ant("/**"))
                 .build()
                 .apiInfo(apiInfo())
-                .globalOperationParameters(
-                        Collections.singletonList(
-                                new ParameterBuilder()
-                                        .name("Authorization")
-                                        .description("Header para Token JWT")
-                                        .modelRef(new ModelRef("string"))
-                                        .parameterType("header")
-                                        .required(false)
-                                        .build()));
+                .globalOperationParameters(params);
 
     }
 
